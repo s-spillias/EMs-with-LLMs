fc22bd85f77e114e3beb07ccbb32c7726c3f75c0
s-spillias <scott.spillias@csiro.au>
2025-09-22 16:15:41 +1000

    fix: Update grazing calculation to include density-dependent inhibition

--- POPULATIONS/POPULATION_0028/INDIVIDUAL_S9QU8RWD/model.cpp
+++ POPULATIONS/POPULATION_0028/INDIVIDUAL_S9QU8RWD/model.cpp
@@ -62,7 +62,7 @@ Type objective_function<Type>::operator() ()
     
     // Equation 2: Phytoplankton growth, grazing, and mortality
     Type growth_P = eps_P * (N_pred(t-1) / (K_eps + N_pred(t-1) + eps)) * U * P_pred(t-1) * exp(-alpha * P_pred(t-1)); // Nutrient-dependent conversion efficiency included in growth term
-    Type grazing = g_Z * pow(Z_pred(t-1), gamma) * P_pred(t-1) / (K_P + P_pred(t-1) + h_Z * P_pred(t-1)); // Grazing loss with predator interference and handling time
+    Type grazing = g_Z * pow(Z_pred(t-1), gamma) * P_pred(t-1) / (K_P + P_pred(t-1) + h_Z * P_pred(t-1) + cp * P_pred(t-1) * P_pred(t-1)); // Grazing loss with density-dependent inhibition, predator interference and handling time
     Type mortality_P = d_P * P_pred(t-1);    // Mortality loss
     Type dP = growth_P - grazing - mortality_P;
     
