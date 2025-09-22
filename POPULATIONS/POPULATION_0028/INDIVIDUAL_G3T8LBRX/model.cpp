cb33c19c7626c1758bb85564e7d7ed3920ec6399
s-spillias <scott.spillias@csiro.au>
2025-09-22 16:01:48 +1000

    fix: Update grazing calculation to use sigmoidal functional response

--- POPULATIONS/POPULATION_0028/INDIVIDUAL_G3T8LBRX/model.cpp
+++ POPULATIONS/POPULATION_0028/INDIVIDUAL_G3T8LBRX/model.cpp
@@ -60,7 +60,7 @@ Type objective_function<Type>::operator() ()
     
     // Equation 2: Phytoplankton growth, grazing, and mortality
     Type growth_P = eps_P * U * P_pred(t-1) * exp(-alpha * P_pred(t-1)); // Growth contribution, including self-shading effect
-    Type grazing = g_Z * pow(Z_pred(t-1), gamma) * P_pred(t-1) / (K_P + P_pred(t-1) + eps); // Grazing loss with predator interference
+    Type grazing = g_Z * pow(Z_pred(t-1), gamma) * pow(P_pred(t-1), h) / (pow(K_P, h) + pow(P_pred(t-1), h) + eps); // Grazing loss with sigmoidal (Type III) functional response
     Type mortality_P = d_P * P_pred(t-1);    // Mortality loss
     Type dP = growth_P - grazing - mortality_P;
     
