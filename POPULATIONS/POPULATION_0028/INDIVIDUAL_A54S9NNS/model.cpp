dcd893a6ac791167b34dfddfd945bc3a6d58aea5
s-spillias <scott.spillias@csiro.au>
2025-09-22 16:38:19 +1000

    feat: Update zooplankton dynamics equation to include variable conversion efficiency

--- POPULATIONS/POPULATION_0028/INDIVIDUAL_A54S9NNS/model.cpp
+++ POPULATIONS/POPULATION_0028/INDIVIDUAL_A54S9NNS/model.cpp
@@ -68,8 +68,8 @@ Type objective_function<Type>::operator() ()
     Type mortality_P = d_P * P_pred(t-1);    // Mortality loss
     Type dP = growth_P - grazing - mortality_P;
     
-    // Equation 3: Zooplankton dynamics (growth via grazing, mortality)
-    Type dZ = eps_Z * grazing - d_Z * Z_pred(t-1);
+    // Equation 3: Zooplankton dynamics (growth via grazing including variable conversion efficiency, mortality)
+    Type dZ = eps_Z * (1 - exp(-mu * P_pred(t-1))) * grazing - d_Z * Z_pred(t-1);
     
     // Equation 4: Nutrient recycling and uptake with saturating recycling efficiency
     // dN/dt = - U * P_pred(t-1) + r*(P_pred(t-1) + Z_pred(t-1))/(1 + beta*(P_pred(t-1) + Z_pred(t-1)))
