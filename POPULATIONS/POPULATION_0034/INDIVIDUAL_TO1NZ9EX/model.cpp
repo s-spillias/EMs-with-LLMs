f70a29f0ae975d8981672171927a902b1bad20cd
s-spillias <scott.spillias@csiro.au>
2025-09-23 15:40:23 +1000

    fix: replace fmax and dlognorm with explicit ternary and dlnorm

--- POPULATIONS/POPULATION_0034/INDIVIDUAL_TO1NZ9EX/model.cpp
+++ POPULATIONS/POPULATION_0034/INDIVIDUAL_TO1NZ9EX/model.cpp
@@ -55,7 +55,7 @@ Type objective_function<Type>::operator() ()
     for(int t=1; t<n; t++){
         // Available coral resources
         Type coral_total_prev = fast_pred(t-1) + slow_pred(t-1) + Type(1e-8);
-        Type free_space = fmax(Type(0.0), Type(100.0) - coral_total_prev);
+        Type free_space = ( (Type(100.0) - coral_total_prev) > Type(0.0) ) ? (Type(100.0) - coral_total_prev) : Type(0.0);
 
         // Functional response for predation (Holling type II-like)
         Type coral_consumed_fast = (attack_fast * cots_pred(t-1) * fast_pred(t-1)) /
@@ -72,25 +72,27 @@ Type objective_function<Type>::operator() ()
         Type slow_growth = regrow_slow * slow_pred(t-1) * (free_space/Type(100.0)) * growth_modifier;
 
         // Coral updates
-        fast_pred(t) = fmax(Type(0.0), fast_pred(t-1) + fast_growth - coral_consumed_fast);
-        slow_pred(t) = fmax(Type(0.0), slow_pred(t-1) + slow_growth - coral_consumed_slow);
+        Type fast_next = fast_pred(t-1) + fast_growth - coral_consumed_fast;
+        fast_pred(t) = (fast_next > Type(0.0)) ? fast_next : Type(0.0);
+
+        Type slow_next = slow_pred(t-1) + slow_growth - coral_consumed_slow;
+        slow_pred(t) = (slow_next > Type(0.0)) ? slow_next : Type(0.0);
 
         // COTS population dynamics
         Type carrying_capacity = cots_K * (fast_pred(t-1) + Type(0.5)*slow_pred(t-1) + Type(1e-8));
         Type logistic_growth = cots_r * cots_pred(t-1) * (Type(1.0) - cots_pred(t-1)/carrying_capacity);
         Type mortality = cots_m * cots_pred(t-1);
 
-        cots_pred(t) = fmax(Type(1e-8),
-                                   cots_pred(t-1) + logistic_growth - mortality +
-                                   cotsimm_dat(t) );
+        Type cots_next = cots_pred(t-1) + logistic_growth - mortality + cotsimm_dat(t);
+        cots_pred(t) = (cots_next > Type(1e-8)) ? cots_next : Type(1e-8);
     }
 
     // ---- LIKELIHOOD ----
     Type nll = 0.0;
     for(int t=0; t<n; t++){
-        nll -= dlognorm(cots_dat(t) + Type(1e-8), log(cots_pred(t) + Type(1e-8)), sd_obs, true);
-        nll -= dlognorm(fast_dat(t) + Type(1e-8), log(fast_pred(t) + Type(1e-8)), sd_obs, true);
-        nll -= dlognorm(slow_dat(t) + Type(1e-8), log(slow_pred(t) + Type(1e-8)), sd_obs, true);
+        nll -= dlnorm(cots_dat(t) + Type(1e-8), log(cots_pred(t) + Type(1e-8)), sd_obs, true);
+        nll -= dlnorm(fast_dat(t) + Type(1e-8), log(fast_pred(t) + Type(1e-8)), sd_obs, true);
+        nll -= dlnorm(slow_dat(t) + Type(1e-8), log(slow_pred(t) + Type(1e-8)), sd_obs, true);
     }
 
     // ---- REPORT SECTION ----
